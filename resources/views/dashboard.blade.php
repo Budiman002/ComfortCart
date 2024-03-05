c9f7c40aeeb11a986ffc20c2fab63905ff63a3d7
Paisal Tanjung <paisaltanjung1102@gmail.com>
2024-03-05 13:01:24 +0700

    catalog

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,150 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+  </head>
+  <body>
+    <div class="catalog">
+      <div>
+        <img src="asset/1. On Sale.png" alt="" />
+      </div>
+      <div class="catalog-1">
+    <img src="{{ asset('asset/image 1.png') }}" alt="" />
+    <img src="{{ asset('asset/image 2.png') }}" alt="" />
+    <img src="{{ asset('asset/image 3.png') }}" alt="" />
+    <img src="{{ asset('asset/image 4.png') }}" alt="" />
+      </div>
+      <div class="text">
+        <div class="merk">
+          <p class="produk">High collar wool sweater</p>
+          <p class="produk">High collar wool sweater</p>
+          <p class="produk">High collar wool sweater</p>
+          <p class="produk">High collar wool sweater</p>
+        </div>
+        
+          <div class="price">
+            <div class="aa price-1">
+              <p><s>Rp 1.399.000</s></p>
+              <p class="diskon">Rp 899.000</p>
+            </div>
+            <div class="aa price-2">
+              <p><s>Rp 1.399.000</s></p>
+              <p class="diskon">Rp 899.000</p>
+            </div>
+            <div class="aa price-3">
+              <p><s>Rp 1.399.000</s></p>
+              <p class="diskon">Rp 899.000</p>
+            </div>
+            <div class="aa price-4">
+              <p><s>Rp 1.399.000</s></p>
+              <p class="diskon">Rp 899.000</p>
+            </div>
+            
+          </div>
+          <div class="eclipse">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+          </div>
+      </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+
+
+      <div class="catalog22">
+        <div>
+          <img src="asset/2. Newest Product.png" alt="" />
+        </div>
+        <div class="catalog-2323">
+        <img src="{{ asset('asset/image 5.png') }}" alt="" />
+        <img src="{{ asset('asset/image 6.png') }}" alt="" />
+        <img src="{{ asset('asset/image 7.png') }}" alt="" />
+        <img src="{{ asset('asset/image 8.png') }}" alt="" />
+        </div>
+        <div class="text">
+          <div class="merk">
+            <p class="produk">High collar wool sweater</p>
+            <p class="produk">High collar wool sweater</p>
+            <p class="produk">High collar wool sweater</p>
+            <p class="produk">High collar wool sweater</p>
+          </div>
+          
+            <div class="price-22">
+              <div class="bb price-1">
+                <p>Rp 1.399.000</p>
+              </div>
+              <div class="bb price-2">
+                <p>Rp 1.399.000</p>
+              </div>
+              <div class="cc price-3">
+                <p>Rp 1.399.000</p>
+              </div>
+              <div class="dd price-4">
+                <p>Rp 1.399.000</p>
+              </div>
+              
+            </div>
+            <div class="eclipse">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+
             </div>
         </div>
-    </div>
-</x-app-layout>
+      </div>
+      </div>
+
+      
+
+
+
+      <div class="catalasto">
+        <div>
+          <img src="asset/3. Other Product.png" alt="" />
+        </div>
+        <div class="catalog-1">
+        <img src="{{ asset('asset/image 9.png') }}" alt="" />
+        <img src="{{ asset('asset/image 10.png') }}" alt="" />
+        <img src="{{ asset('asset/image 11.png') }}" alt="" />
+        <img src="{{ asset('asset/image 12.png') }}" alt="" />
+        </div>
+        <div class="text">
+          <div class="merk">
+            <p class="produk">High collar wool sweater</p>
+            <p class="produk">High collar wool sweater</p>
+            <p class="produk">High collar wool sweater</p>
+            <p class="produk">High collar wool sweater</p>
+          </div>
+          
+            <div class="price-22">
+              <div class="price-1">
+                <p>Rp 1.399.000</p>
+              </div>
+              <div class="aa price-2">
+                <p>Rp 1.399.000</p>
+              </div>
+              <div class="aa price-3">
+                <p>Rp 1.399.000</p>
+              </div>
+              <div class="aa price-4">
+                <p>Rp 1.399.000</p>
+              </div>
+              
+            </div>
+            <div class="eclipse">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            <img src="{{ asset('asset/Eclipse.png') }}" alt="">
+            </div>
+        </div>
+
+
+
+
+  </body>
+</html>
