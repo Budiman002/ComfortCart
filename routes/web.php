3a2800652153f9eb9f3b77c77f3c4e0a5bed6712
Tinnne <austin.kane1812@gmail.com>
2024-03-14 23:46:27 +0700

    Final HomePage

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RegisterController;
@@ -17,13 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomePageController::class, 'index']);
 //Home Page
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/home', [HomePageController::class, 'index']);
 
 Route::get('/admin-panel', [AdminController::class,'admin'])->middleware('App\Http\Middleware\IsAdmin');
 
